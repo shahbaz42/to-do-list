9e85818f72356a2b73b4dc91784e32724ebcb9cf
shahbaz ali <shahbaz_ali@outlook.in>
2021-08-23 00:50:01 +0530

    added handler to recieve input, and append to an array; used to localeDatestring to print date;

--- app.js
+++ app.js
@@ -3,14 +3,32 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true})); //linking bodyparser with express
 app.set('view engine', 'ejs');
 
+var tasks = [];
+
 app.get("/", function(req, res){
-  const date = new Date();
-  var day = date.getDay() ;
-  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  res.render('list', {dayName : days[day]});
+  const today = new Date();
+
+  var options = {
+    weekday: 'long',
+    day: 'numeric',
+    month : 'long'
+  };
+
+  var formattedDate = today.toLocaleDateString("en-US", options);
+  res.render('list', {dayName : formattedDate, taskList : tasks});
 });
+
+
+
+app.post("/", function(req, res){
+  console.log(req.body.taskName);
+  tasks.push(req.body.taskName);  // appends submitted task to tasks list
+  res.redirect("/");
+});
+
 
 app.listen(8000,function(){
   console.log("Server started on port 3000");
