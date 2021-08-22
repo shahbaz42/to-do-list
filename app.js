68a700c755af39adabe5bf652b1a9ec2c61e3cfc
shahbaz ali <shahbaz_ali@outlook.in>
2021-08-22 22:26:49 +0530

    make simple weekend finder app

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const date = new Date();
+
 app.get("/", function(req, res){
-  res.send("Hello");
+  var day = date.getDay() ;
+  if (day==0 || day==6){
+    res.send("Today is a weekend!!");
+  }else{
+    res.send("Today is a work day!!");
+  }
 });
 
 app.listen(8000,function(){
