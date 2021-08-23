192d68f6290571b69677608eb59f7a6e5329d8e2
shahbaz ali <shahbaz_ali@outlook.in>
2021-08-23 15:31:53 +0530

    add about page; optimised for mobile

--- app.js
+++ app.js
@@ -38,8 +38,11 @@ app.post("/", function(req, res){
   }
 });
 
+app.get("/about", function(req, res){
+  res.render("about");
+});
 
 
-app.listen(3000,function(){
-  console.log("Server started on port 3000");
+app.listen(8000,function(){
+  console.log("Server started on port 8000");
 });
